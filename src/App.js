a39c0d0d0db8a291127dcd68eb4473aad0a7dbe7
Priyom Saha <priyom1499@gmail.com>
2020-07-17 10:13:52 +0530

    error for sending avdojo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+/*import React, { useState, useEffect } from 'react';
 import './App.css';
 import { auth} from './firebase'
 import SignIn from './Component/SignIn'
@@ -33,4 +33,49 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
+import React, { Component } from 'react'
+import firebase from './firebase'
+export class App extends Component {
+
+  handleClick = () => {
+    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
+    var number = '+91987654321';
+    var appVerifier = window.recaptchaVerifier;
+    firebase.auth().signInWithPhoneNumber(number, appVerifier).then(function (e) {
+      var code = '123654';
+
+
+      if (code === null) return;
+
+
+      e.confirm(code).then(function (result) {
+        console.log(result.user);
+
+        document.querySelector('label').textContent += result.user.phoneNumber + "Number verified";
+
+      }).catch(function (error) {
+        console.error(error);
+
+      });
+
+    })
+      .catch(function (error) {
+        console.error(error);
+
+      });
+  }
+  render() {
+    return (
+      <div>
+        <label></label>
+
+        <div id="recaptcha"></div>
+
+        <button onClick={this.handleClick}>Click</button>
+      </div>
+    )
+  }
+}
+
+export default App
--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,16 @@
-  
-import firebase from 'firebase/app';
-import 'firebase/auth'
+
+/*import 'firebase/auth'
 import 'firebase/firestore';
 
 // Your Web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyAG3GUwgc5ExCCI7LgnJ-07iYp1iSGnmZs",
-    authDomain: "fir-authtest-a6924.firebaseapp.com",
-    databaseURL: "https://fir-authtest-a6924.firebaseio.com",
-    projectId: "fir-authtest-a6924",
-    /*storageBucket: "fir-authtest-a6924.appspot.com",
-    messagingSenderId: "411960892184",
-    appId: "1:411960892184:web:5ae980729e643b8f519c6d"*/
+  apiKey: "AIzaSyAG3GUwgc5ExCCI7LgnJ-07iYp1iSGnmZs",
+  authDomain: "fir-authtest-a6924.firebaseapp.com",
+  databaseURL: "https://fir-authtest-a6924.firebaseio.com",
+  projectId: "fir-authtest-a6924",
+  storageBucket: "fir-authtest-a6924.appspot.com",
+  messagingSenderId: "411960892184",
+  appId: "1:411960892184:web:5ae980729e643b8f519c6d"
   };
 
 // Initialize Firebase
@@ -19,5 +18,20 @@ export const firebaseApp = firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
 export const db = firebase.firestore();
 export const google_provider = new firebase.auth.GoogleAuthProvider();
-export const facebook_provider = new firebase.auth.FacebookAuthProvider();
+export const facebook_provider = new firebase.auth.FacebookAuthProvider();*/
+import firebase from 'firebase/app';
+
+// Web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAG3GUwgc5ExCCI7LgnJ-07iYp1iSGnmZs",
+  authDomain: "fir-authtest-a6924.firebaseapp.com",
+  databaseURL: "https://fir-authtest-a6924.firebaseio.com",
+  projectId: "fir-authtest-a6924",
+  storageBucket: "fir-authtest-a6924.appspot.com",
+  messagingSenderId: "411960892184",
+  appId: "1:411960892184:web:5ae980729e643b8f519c6d"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export default firebase;
 
